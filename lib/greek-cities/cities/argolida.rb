3506935cff3e031b5e44403dd1fbbae3136628ca
Alex <avlonitis@msn.com>
2016-09-30 23:37:35 +0100

    added argolida

---
+++ lib/greek-cities/cities/argolida.rb
@@ -0,0 +1,10 @@
+module GreekCities
+  module Cities
+    ARGOLIDA = [
+      ['Άργος - Μυκήνες', 'argos-mikines-ARG'],
+      ['Επίδαυρος', 'epidavros-ARG'],
+      ['Ερμιονίδα', 'ermionida-ARG'],
+      ['Ναύπλιο', 'nafplio-ARG']
+    ].freeze.clone
+  end
+end
--- lib/greek-cities/city.rb
+++ lib/greek-cities/city.rb
@@ -1,10 +1,12 @@
 require 'greek-cities/cities/aitoloakarnania'
+require 'greek-cities/cities/argolida'
 
 module GreekCities
   class City
     def self.get_cities(code)
       case code
-      when 'AIT', 'ait' then GreekCities::Cities::AIT
+      when 'AIT', 'ait' then GreekCities::Cities::AITOLOAKARNANIA
+      when 'ARG', 'arg' then GreekCities::Cities::ARGOLIDA
       else
         puts 'city not found'
       end
